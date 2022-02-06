debfa54fbfedb6988b817e35a9232c92d51c61af
NovaLord <hunter.oliver0512@protonmail.com>
2022-02-06 08:41:00 +0000

    contact form php attempt

---
+++ php/contactform.php
@@ -0,0 +1,15 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $from = $_POST['email'];
+    $message = $_POST['message'];
+    $businessType = $_POST['business'];
+
+$to = "hunter.oliver0512@protonmail.com";
+$headers = "From: ".$from." on portfolio contact form";
+$messageSent = "Contact form message from ".$name." - ".$businessType."\n\n".$message;
+
+mail($to,$headers,$messageSent);
+header("Location: contact.html?mailsend")
+}
